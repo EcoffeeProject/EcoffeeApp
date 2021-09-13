c4abc1b87d6580545898706e94cccd369b476dfb
Wise-eun <hee1871@naver.com>
2021-09-13 17:09:45 +0900

    upload ecoffee

---
+++ app/src/main/java/com/example/ecoffe/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.ecoffe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceStare) {
+        super.onCreate(savedInstanceStare);
+        setContentView(R.layout.login);
+
+    }
+
+}
